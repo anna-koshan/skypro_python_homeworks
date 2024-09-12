e953744db56b9a3eb2ddf2bc4604704517938bbb
anna-koshan <anna.koshan1506@gmail.com>
2024-09-12 12:38:41 +0300

    add third homework

---
+++ lesson3 copy/address.py
@@ -0,0 +1,13 @@
+class Address:
+   
+    def __init__(self, index, city, street, home, room):
+       self.index = index
+       self.city = city
+       self.street = street
+       self.home = home
+       self.room = room
+
+    def get_Info (self):
+        return f'{self.index}, {self.city}, {self.street}, {self.home} - {self.room}'
+
+    
