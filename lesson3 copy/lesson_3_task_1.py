08fef7781498dc390aa809d9eecc86ca035874c1
Anna_Mulya <165191725+anna-koshan@users.noreply.github.com>
2024-09-12 12:41:19 +0300

    Delete lesson3 copy directory

--- lesson3 copy/lesson_3_task_1.py
+++
@@ -1,6 +0,0 @@
-from user import User
-
-my_user = User("Аня", "Мулянова")
-my_user.get_first_name()
-my_user.get_last_name()
-my_user.get_full_name()
--- lesson3 copy/lesson_3_task_3.py
+++
@@ -1,7 +0,0 @@
-from mailing import Mailing 
-from address import Address
-
-to_address = Address("123456", "Москва", "ул. Советская", "д. 2", "45")
-from_address = Address("789564", "Тюмень", "ул. Наличная", "д. 23", "56")
-my_mailing = Mailing(to_address, from_address, 1500, "1245789635")
-my_mailing.sayAddress()
